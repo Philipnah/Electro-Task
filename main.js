8a580c0a035009b47c9b1a87097efbdcef284b0a
Philipnah <Philip@han.dk>
2019-11-01 17:22:09 +0100

    added basic start program commands

---
+++ main.js
@@ -0,0 +1,20 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow
+
+
+app.on("ready", function(){
+     mainWindow = new BrowserWindow({});
+
+     //load index.html
+     mainWindow.loadURL(url.format({
+          pathname: path.join(__dirname, "index.html"),
+          protocol: "file:",
+          slashes: true
+     }));
+     
+});
