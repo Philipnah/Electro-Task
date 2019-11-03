029be1453c59664d84b71b13d61d8d7b77a29583
Philipnah <Philip@han.dk>
2019-11-03 17:57:41 +0100

    added new functions

--- Scripts/fileSystem.js
+++ Scripts/fileSystem.js
@@ -3,12 +3,27 @@ const fs = require("fs");
 var userDocumentsPath = "D:/Dokumenter";
 var userNotePath = userDocumentsPath + "/electronnote_docs/";
 
-var currentfileName = "file.txt";
+var currentfileName = "note1.txt";
 var userNoteTXT = userNotePath + currentfileName;
 
+fs.readdir(userNotePath, (err, files) => {
+     files.forEach(file => {
+       console.log(file);
+     });
+   });
 
 function newFile() {
      console.log("New note will be created!")
+     var newNote = document.getElementById("input").value
+     console.log(newNote);
+     
+     var currentfileName = "message.txt";
+     var userNoteTXT = userNotePath + currentfileName;
+     fs.writeFile(userNoteTXT, newNote, (err) => {
+          if (err) throw err;
+          console.log('The "data to write" was written to file!');
+        });
+
 }
 
 
@@ -20,12 +35,11 @@ function readFile() {
           console.log(data);
           var content = data;
 
-          document.getElementById("notes").innerHTML = content;
-          });
+          document.getElementById("notes").innerHTML = content + "<br>";
+     });
 
      
 }
-
 
 
 readFile()
